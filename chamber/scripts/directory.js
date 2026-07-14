b73892c44f1f2c9aafb034a54ce477ea97688e0c
josephiu <iu.joseph37@gmail.com>
2026-07-14 18:09:31 +0400

    icon

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -8,23 +8,23 @@ const cards= document.querySelector(".gridcard");
 
 
 
-async function getMembersData() {
+async function getCompaniesData() {
     const response = await fetch(url);
     const data = await response.json(); 
     //refrencing the data object of the members array                                          
-    displayMembers(data.companies);                                                                                    
+    displayCompanies(data.companies);                                                                                    
 
     // console.log(data.companies); // temporary testing of data retreival
   
 }
   
-getMembersData();
+getCompaniesData();
 
 
 // //build a display cards for each item in our members array 
- const displayMembers = (members) => {
+ const displayCompanies=(companies) => {
 
-    members.forEach((member) => {   
+    companies.forEach((company) => {   
 
         let card = document.createElement("section");
 
@@ -43,22 +43,20 @@ getMembersData();
 
         //  Build the h2 content out to show the member's 
 
-        cName.textContent = `${member.company_name}`; 
-        tele.textContent = `${member.phone_number}`;
-        yeahJoin.textContent = `Joined year: ${member.joinYear}`;
-        mLevel.textContent = `Membership level: ${member.membershipLevel}`;
-        address.textContent = `${member.address}`;
+        cName.textContent = `${company.company_name}`; 
+        tele.textContent = `${company.phone_number}`;
+        yeahJoin.textContent = `Joined year: ${company.joinYear}`;
+        mLevel.textContent = `Membership level: ${company.membershipLevel}`;
+        address.textContent = `${company.address}`;
 
-        web.innerHTML = `<a href="${member.website}">${member.website}</a>`;
+        web.innerHTML = `<a href="${company.website}">${company.website}</a>`;
 
 
-        cLogo.setAttribute("src", member.icon);
-        cLogo.setAttribute("alt",`Logo of ${member.company_name}`);
+        cLogo.setAttribute("src", company.icon);
+        cLogo.setAttribute("alt",`Logo of ${company.company_name}`);
         cLogo.setAttribute('loading', 'lazy');
         cLogo.setAttribute('width', '150');
         cLogo.setAttribute('height', '150');
-
-
 
 
         card.appendChild(cLogo);
@@ -68,9 +66,6 @@ getMembersData();
         card.appendChild(tele);
         card.appendChild(address);
         card.appendChild(web);
-
-
-
         cards.appendChild(card);       
     
     });
